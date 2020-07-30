8682437960cb6a728dd07cb14e5f1a96063bfe9d
Ben Cook <jbenjamincook@gmail.com>
2020-07-30 11:49:33 -0500

    clean up format

--- simple-track.py
+++ simple-track.py
@@ -17,12 +17,14 @@ def track(
     class_index: int = 1,  # Track people by default
 ) -> None:
     """Track the objects for a specific class in a given video"""
+    # Initialization
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = fasterrcnn_resnet50_fpn(pretrained=True).eval().to(device)
-
+    reader = imageio.get_reader(video_path)
+    # Tracking loop
     active_tracklets = []
     finished_tracklets = []
-    for i, frame in enumerate(imageio.get_reader(video_path)):
+    for i, frame in enumerate(reader):
         # Detection
         x = to_tensor(frame).to(device)
         result = model(x[None])[0]
@@ -31,30 +33,39 @@ def track(
         mask = torch.logical_and(
             result["labels"] == class_index, result["scores"] > score_threshold
         )
-        boxes = np.round(result["boxes"][mask].data.cpu().numpy())
+        boxes = result["boxes"][mask].data.cpu().numpy()
         prev_indices = boxes_indices = []
         if i > 0:
             # Pairwise cost: euclidean distance between boxes
             cost = np.linalg.norm(prev_boxes[:, None] - boxes[None], axis=-1)
             # Bipartite matching
             prev_indices, boxes_indices = linear_sum_assignment(cost)
-
         # Add matches to active tracklets
         for prev_idx, box_idx in zip(prev_indices, boxes_indices):
-            active_tracklets[prev_idx]["boxes"].append(boxes[box_idx].tolist())
-        # Remove lost tracklets
+            active_tracklets[prev_idx]["boxes"].append(
+                np.round(boxes[box_idx]).astype("int").tolist()
+            )
+        # Finalize lost tracklets
         lost_indices = set(range(len(active_tracklets))) - set(prev_indices)
         for lost_idx in sorted(lost_indices, reverse=True):
             finished_tracklets.append(active_tracklets.pop(lost_idx))
-        # Add new tracklets
+        # Activate new tracklets
         new_indices = set(range(len(boxes))) - set(boxes_indices)
         for new_idx in new_indices:
-            active_tracklets.append({"start": i, "boxes": [boxes[new_idx].tolist()]})
-
-        # Predict next
+            active_tracklets.append(
+                {"start": i, "boxes": [np.round(boxes[new_idx]).astype(int).tolist()]}
+            )
+        # "Predict" next frame for comparison
         prev_boxes = np.array([tracklet["boxes"][-1] for tracklet in active_tracklets])
     with open(output_path, "w") as f:
-        f.write(json.dumps(finished_tracklets + active_tracklets))
+        f.write(
+            json.dumps(
+                {
+                    "fps": reader.get_meta_data()["fps"],
+                    "tracklets": finished_tracklets + active_tracklets,
+                }
+            )
+        )
 
 
 if __name__ == "__main__":
